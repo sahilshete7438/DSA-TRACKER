570dc6f420819e39ddfef5c1d0d7946be8276dfe
sahil shete <sahilshete875@gmail.com>
2025-04-21 21:28:44 +0530

    Initial commit for backend

--- dssa-tracker/dssa-tracker-backend/server.js
+++ dssa-tracker/dssa-tracker-backend/server.js
@@ -7,16 +7,19 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/dsa-tracker')
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+// MongoDB Atlas Connection
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 // User Schema
 const userSchema = new mongoose.Schema({
@@ -567,5 +570,5 @@ app.get('/user/:username', async (req, res) => {
 
 // Start Server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
